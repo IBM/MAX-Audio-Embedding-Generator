f070ebc8a50279cc90a2403b34f7fa17a4c6c8c1
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-08-24 00:38:42 -0700

    Integration Tests (#13)
    * switched to base image and added tests
    
    * added sleep to .travis.yml
    
    * switch to requests

---
+++ tests/test.py
@@ -0,0 +1,28 @@
+import pytest
+import requests
+
+
+def test_response():
+
+    model_endpoint = 'http://localhost:5000/model/predict'
+    file_path = 'assets/car-horn.wav'
+
+    with open(file_path, 'rb') as file:
+        file_form = {'audio': (file_path, file, 'audio/wav')}
+        r = requests.post(url=model_endpoint, files=file_form)
+
+    assert r.status_code == 200
+
+    response = r.json()
+
+    assert response['status'] == 'ok'
+
+    assert len(response['predictions'][0]['embedding']) == 4
+    assert len(response['predictions'][0]['embedding'][0]) == 128
+    assert len(response['predictions'][0]['embedding'][1]) == 128
+    assert len(response['predictions'][0]['embedding'][2]) == 128
+    assert len(response['predictions'][0]['embedding'][3]) == 128
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
